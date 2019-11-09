2c2bc264d84e0ffffd54815258872f94b5e0c886
Ubuntu <ubuntu@ip-172-31-23-24.us-west-2.compute.internal>
2019-11-09 21:16:06 +0000

    client is ready

--- client.sh
+++ client.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-taskset 0x01 iperf3 -c 172.31.23.24 -p 5000 -u -b 5G > logs/client01.txt &
-taskset 0x02 iperf3 -c 172.31.23.24 -p 5001 -u -b 5G > logs/client02.txt &
-watch -n 1 -d ifconfig ens3
+EXPNAME=$1
+DURATION=$2
+ROLE="$(cat ~/name.txt)"
+echo $ROLE
+
+taskset 0x01 iperf3 -c 172.31.23.24 -p 5000 -t $DURATION -u -b 5G >> logs/${EXPNAME}_${ROLE}_client01.txt &
+taskset 0x02 iperf3 -c 172.31.23.24 -p 5001 -t $DURATION -u -b 5G >> logs/${EXPNAME}_${ROLE}_client02.txt &
+sar -n DEV 1 >> logs/${EXPNAME}_${ROLE}_sar.txt &
+sleep $DURATION
+#watch -n 1 -d ifconfig ens3
 # while ifconfig ens3 | grep 'TX bytes'; do sleep 10; done
