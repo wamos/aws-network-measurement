0e1d133f000b55afe1a1d9717acceaf097e1504c
Ubuntu <ubuntu@ip-172-31-42-64.us-west-2.compute.internal>
2019-11-10 02:16:08 +0000

    logs and logs

--- client.sh
+++ client.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 # EXPNAME=$1
 # DURATION=$2
 ROLE="$(cat ~/name.txt)"
 echo $ROLE
+
+pkill iperf
 
 for i in "$@"
 do
@@ -37,17 +40,18 @@ echo $DURATION, $EXPNAME
 pktsize=${pktsize:-1}
 
 if [[ $UDP ]]; then
-	taskset 0x01 iperf3 -c 172.31.23.24 -p 5000 -t $DURATION -l $pktsize -u -b 5G >> logs/"$EXPNAME"_"$ROLE"_client01.txt &
-	taskset 0x02 iperf3 -c 172.31.23.24 -p 5001 -t $DURATION -l $pktsize -u -b 5G >> logs/"$EXPNAME"_"$ROLE"_client02.txt &
+	taskset 0x01 iperf3 -c 172.31.23.24 -p 5000 -t $DURATION -l $pktsize -u -b 5G > logs/"$EXPNAME"_"$ROLE"_client01.txt &
+	taskset 0x02 iperf3 -c 172.31.23.24 -p 5001 -t $DURATION -l $pktsize -u -b 5G > logs/"$EXPNAME"_"$ROLE"_client02.txt &
 elif [[ $TCP ]]; then
-	taskset 0x01 iperf3 -c 172.31.23.24 -p 5000 -t $DURATION -l $pktsize -b 5G >> logs/"$EXPNAME"_"$ROLE"_client01.txt &
-	taskset 0x02 iperf3 -c 172.31.23.24 -p 5001 -t $DURATION -l $pktsize -b 5G >> logs/"$EXPNAME"_"$ROLE"_client02.txt &
+	taskset 0x01 iperf3 -c 172.31.23.24 -p 5000 -t $DURATION -l $pktsize -b 5G > logs/"$EXPNAME"_"$ROLE"_client01.txt &
+	taskset 0x02 iperf3 -c 172.31.23.24 -p 5001 -t $DURATION -l $pktsize -b 5G > logs/"$EXPNAME"_"$ROLE"_client02.txt &
 else
 	echo "Pick a protocol"
 	exit 1
 fi
 
-sar -n DEV 1 >> logs/"$EXPNAME"_"$ROLE"_sar.txt &
+sar -n DEV 1 > logs/"$EXPNAME"_"$ROLE"_sar.txt &
+sar -P 0,1 > logs/"$EXPNAME"_"$ROLE"_cpu.txt &
 sleep $DURATION
 sleep 1
 pkill sar
